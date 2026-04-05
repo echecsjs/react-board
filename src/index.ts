8cccc47de564c8b9c9254b2cc7e2deb1cf17bd8a
Adrian de la Rosa <github@brick.email>
2026-04-05 16:39:56 +0200

    export PromotionDialog from package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { default as Board } from './board.js';
 export { DEFAULT_PIECES } from './pieces/index.js';
+export { PromotionDialog } from './promotion-dialog.js';
 export { squareCoords } from './utilities.js';
 
 export type {
@@ -10,4 +11,9 @@ export type {
   PieceKey,
   PieceSet,
 } from './types.js';
+
+export type {
+  PromotionDialogProps,
+  PromotionPiece,
+} from './promotion-dialog.js';
 export type { SquareCoords } from './utilities.js';
